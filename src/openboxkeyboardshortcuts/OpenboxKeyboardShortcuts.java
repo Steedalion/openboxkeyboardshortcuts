7a18ce4120c51146f0e9937174a5684de1215d57
Steed <alex.steed88@gmail.com>
2018-10-30 09:39:13 +0900

    Initial commit - Added heading before and after. - Still requires some manual post processing.

--- src/openboxkeyboardshortcuts/OpenboxKeyboardShortcuts.java
+++ src/openboxkeyboardshortcuts/OpenboxKeyboardShortcuts.java
@@ -17,7 +17,8 @@ import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 /**
- *
+ * This program parses the Openbox configuration file to a Sphinx table.
+ * 
  * @author csteed
  */
 public class OpenboxKeyboardShortcuts {
@@ -27,44 +28,53 @@ public class OpenboxKeyboardShortcuts {
      */
     public static void main(String[] args) throws ParserConfigurationException {
 
-        Document doc;
+        final String table= "\n============================= ========================\n";
+        Document xmlDocument;
         File inputXML = new File("lxqt-rc.xml"); inputXML.setReadOnly();
-        doc = loadOpenboxFile(inputXML);
-        doc.getDocumentElement().normalize();
-        PrintWriter writer = createOutputFile("keyboard_shorcuts.rst");
+        xmlDocument = loadOpenboxFile(inputXML);
+        xmlDocument.getDocumentElement().normalize();
+        PrintWriter fileWriter = createOutputFile("openbox_keyboard.rst");
 
-        Node currentNode = doc.getDocumentElement().getElementsByTagName("keyboard").item(0).getFirstChild();
+        Node currentNode = xmlDocument.getDocumentElement().getElementsByTagName("keyboard").item(0).getFirstChild();
+        
         while (currentNode != null) {
-            //System.out.println("sss"+currentNode);
 
-//            
             if ((currentNode.getNodeType() == Node.ELEMENT_NODE)) {
                 Element el = (Element) currentNode;
                 if (el.getTagName() == "keybind") {
 
-                    String outputLine;
-                    outputLine = el.getElementsByTagName("action").item(0).getAttributes().getNamedItem("name").getTextContent() + "\t"
-                            + el.getAttribute("key")
-                            + "\n";
-                    writer.write(outputLine);
+                    StringBuilder outputLine = new StringBuilder();
+                    String action = el.getElementsByTagName("action").item(0).getAttributes().getNamedItem("name").getTextContent();
+                    String keys = el.getAttribute("key");
+                    
+                    outputLine.append(action);
+                    while (outputLine.length()<30) {
+                        outputLine.append(" ");
+                    }
+                    outputLine.append(keys);
+                    
+                    
+                    fileWriter.write("\n"+ outputLine.toString() + "\n");
                 } 
 
 //                
             }else if (currentNode.getNodeType() == Node.COMMENT_NODE) {
                     Comment comment = (Comment) currentNode;
+                    String heading = table+"\n**"+removeSpaceBeforeAndAfter(comment.getTextContent())+"**\n"+table;
+                    
 
-                    System.out.println("**"+comment.getTextContent()+"**");
-                    writer.write("**"+comment.getTextContent()+"**"+ "\n\n");
+                    System.out.println(heading);
+                    fileWriter.write(heading);
                 }
-            writer.flush();
+            fileWriter.flush();
             currentNode = currentNode.getNextSibling();
         }
-       writer.close();
+       fileWriter.close();
        
 
     }
 
-    public static Document loadOpenboxFile(File inputFile) {
+    private static Document loadOpenboxFile(File inputFile) {
         
         try {
             DocumentBuilderFactory dfact = DocumentBuilderFactory.newInstance();
@@ -83,7 +93,7 @@ public class OpenboxKeyboardShortcuts {
 
     }
 
-    public static PrintWriter createOutputFile(String fileName) {
+    private static PrintWriter createOutputFile(String fileName) {
         try {
             PrintWriter writer = new PrintWriter(fileName);
             return writer;
@@ -92,5 +102,15 @@ public class OpenboxKeyboardShortcuts {
         }
         return null;
     }
+    
+    private static String removeSpaceBeforeAndAfter(String input)
+    {
+        while(input.startsWith(" "))
+        {
+         input = input.replaceFirst(" ", "");
+        }
+        input = input.trim();
 
+        return input;
+    }
 }
